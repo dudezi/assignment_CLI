85596462db63095ad29965baa4ca6e437c7ba5bb
JeaHukLim <a01036952858@gmail.com>
2024-08-30 00:02:31 +0900

    refactor:App추가를 통해 구조개선

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("== 게시판 앱 ==");
-
-        while (true) {
-            System.out.print("명령) ");
-            String command = sc.nextLine();
-
-            if (command.trim().equals("종료")) {
-                break;
-            } else if (command.trim().equals("등록")) {
-                System.out.print("제목 : ");
-                String subject = sc.nextLine();
-                System.out.print("내용 : ");
-                String content = sc.nextLine();
-            }
-        }
+        new App(sc).run();
         sc.close();
     }
 }
